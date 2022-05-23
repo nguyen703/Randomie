a82a9245f73f323d3999b6270314ad425b4aa30a
nguyenngo703 <nguyenngohoai98@gmail.com>
2022-05-23 11:34:47 +0700

    Initial Commit

---
+++ Randomie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Randomie
+//
+//  Created by Nguyen NGO on 23/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
